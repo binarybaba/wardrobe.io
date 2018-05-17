c1ed9def516602a03d3e8bd95014529b27c66792
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2018-05-17 20:01:16 +0530

    add tools and stuff

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = function(env){
+    console.log(env);
+    var config = {
+        context: __dirname,
+        entry: "./src/App.js",
+        devtool: env === "production" ? "source-map" : "cheap-eval-source-map",
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        devServer: {
+            publicPath: '/public/',
+            filename: "bundle.js",
+            port:9000
+        },
+        stats: {
+            colors: true,
+            reasons: true,
+            chunks: true,
+        },
+        resolve: {
+            extensions: ['.js', '.json'],
+        },
+        module: {
+            rules: [{
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                test: /\.js$/,
+            }, {
+                loader: 'html-loader',
+                test: /\.html$/,
+            },{
+                loader: 'babel-loader',
+                test: /\.js$/,
+            },{
+                loader:'css-loader',
+                test:/\.css/
+            },{
+                loader:'url-loader',
+                test:/\.png$/,
+                exclude: /node_modules/,
+            }],
+        },
+        "plugins": [
+            new webpack.ProvidePlugin({
+                "window.jQuery": "jquery"
+            }),
+        ]
+    };
+    if(env === "production") {
+        config.plugins.push(new UglifyJSPlugin({
+            sourceMap: true
+        }))
+    }
+    return config;
+};
